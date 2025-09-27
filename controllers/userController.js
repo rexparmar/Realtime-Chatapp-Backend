dd6c7568896d6077a023e1d4bb6ae27f01014ae8
rexparmar <parmarrex114@gmail.com>
2025-09-27 16:23:30 -0400

    hello

---
+++ controllers/userController.js
@@ -0,0 +1,72 @@
+import cloudinary from "../lib/cloudinary.js";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from 'bcryptjs'
+
+// Signup new User
+export const signup = async (req, res) =>{
+    const {fullName, email, password, bio} = req.body;
+    try{
+        if(!fullName || !email || !password || !bio){
+            return res.json({success:false, message:"Missing Details"})
+        }
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.json({success:false, message:"Account already exists"});
+        }
+        const sale = await bcrypt.genSalt(10)
+        const hashedPassword = bcrypt.hash(password, salt)
+
+        const newUser = await User.create({
+            fullName,email,password:hashedPassword,bio
+        })
+        const token = generateToken(newUser._id);
+        res.json({success:true, userData:newUser, token, message:"Account created Successfully!"})
+    }catch(error){
+        res.json({success:false, message:"New User creation failed!"})
+        console.log(error)
+    }
+}
+
+// Function for login
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        const userData = User.findOne({email})
+        
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password)
+        if(!isPasswordCorrect){
+            res.json({success:false, message:"Password invalid"});
+        }
+        const token = generateToken(userData._id);
+        res.json({success:true, userData, token, message : "Login Success"})
+    }catch(error){
+        res.json({success:false, message:"Login failed!"})
+        console.log(error)
+    }
+}
+
+// COntroller to check if user is authenticated
+export const checkAuth = (req, res) =>{
+    res.json({success:true, user:req.user})
+}
+
+// Function to update user profile detail
+export const updateProfile = async (req, res) =>{
+    try {
+        const{profilePic ,fullName, bio} = req.body;
+        const userId = req.user._id;
+        let updatedUser;
+        
+        if(!profilePic){
+            await User.findByIdAndUpdate(userId, {bio,fullName}, {new: true})
+        }else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+            updatedUser=await User.findByIdAndUpdate(userId, {profilePic : upload.secure_url, bio, fullName}, {new: true})
+        }
+        res.json({success:true, user: updatedUser})
+    } catch (error) {
+        res.json({success:false, message: error.message});
+    }
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import "dotenv/config";
+import cors from "cors"
+import http from "http";
+import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoute.js';
+
+// Create express app and http server
+const app = express();
+const server = http.createServer(app);
+
+// Middleware setup
+app.use(express.json({limit:"4mb"}))
+app.use(cors())
+
+app.use("/api/status", (req, res)=>res.send("Server is live!"))
+
+// Routes Setup
+app.use("/api/auth", userRouter)
+
+// Connect to db
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>console.log("Server is running on port : " + PORT));
+
