38c7b53f034a6eff39577c19c6f516af54907f02
rexparmar <parmarrex114@gmail.com>
2025-10-03 17:43:15 -0400

    Route updated for marking msg as read.

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -6,7 +6,7 @@ const messageRouter = express.Router();
 
 messageRouter.get("/users",protectRoute, getUsersForSideBar)
 messageRouter.get("/:id",protectRoute, getMessage)
-messageRouter.put("mark/:id",protectRoute, markMessageAsSeen)
+messageRouter.put("/mark/:id",protectRoute, markMessageAsSeen)
 messageRouter.post("/send/:id",protectRoute, sendMessage)
 
 export default messageRouter;
